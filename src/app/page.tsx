157574d2b913ee3a80c20ee40d7c6b04756e3b6e
viduraperera <laumayangaperera@gmail.com>
2024-07-22 02:26:25 +0530

    fix: remove style file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import LandingPageComponent from "@/components/landing-page/LandingPageComponent";
 
 export default function Home() {
